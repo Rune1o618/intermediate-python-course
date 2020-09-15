1e244eebaa66162c0b51a7a9b8fac65a2230dc1b
Rune1o618 <abhijay.hazarika@gmail.com>
2020-09-15 21:33:33 +0530

    Now we Rolling Two Die

--- dice_roller.py
+++ dice_roller.py
@@ -1,5 +1,16 @@
+import random #Generating Random Numbers
+
+
+
 def main():
-  print('You rolled a die')
+  dice_sum  = 0  
+  dice_rolls = 2
+  for i in range(0, dice_rolls):
+    roll = random.randint(1,6)
+    dice_sum = dice_sum + roll
+    print("You have rolled a {0}.".format(roll))
+    print("You have a total dice sum of {0}!".format(dice_sum))
+    
 
 if __name__== "__main__":
   main()
